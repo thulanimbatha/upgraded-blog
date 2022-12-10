f5ec65f4273b2ca10f6a1b0ad1e0bd0681a2cb5d
thulanimbatha <thulanimbatha06@gmail.com>
2022-12-10 12:50:58 +0200

    version 2.2.2

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+POSTS_URL = "https://api.npoint.io/43a583d3b3d6654debfc"
+posts = requests.get(POSTS_URL).json()
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_posts=posts)
 
 @app.route('/about')
 def about():
@@ -13,6 +17,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for post in posts:
+        if post['id'] == index:
+            requested_post = post
+    return render_template('post.html', post=requested_post)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
